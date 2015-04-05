eb589a85ad6260b88a91192dd6c16d9b62b655aa
lerman <joachimlerman@gmail.com>
2015-04-05 11:18:24 -0700

    added the print method for a mancala board for testing

--- MancalaBoard.java
+++ MancalaBoard.java
@@ -7,7 +7,7 @@ public class MancalaBoard
     private Pit[ ] mancalaBoard;
     public Pit[] getBoard()
     {
-    return mancalaBoard;
+    	return mancalaBoard;
     }
     public MancalaBoard(int initialStones)
     {
@@ -23,34 +23,54 @@ public class MancalaBoard
          mancalaBoard[PLAYER_A_MANCALA] = new MancalaPit(0, true);  //WAS 6
          mancalaBoard[PLAYER_B_MANCALA] = new MancalaPit(0, false);	// WAS 13
     }
-    
-   public void move(int mancalaPitIndex)
-   {
-	   //TODO make sure that a rock isnt placed in the enemy mancala
-	   int stonesInHand = mancalaBoard[mancalaPitIndex].getStones();
-       int mancalaPitIndex2=0;
-	   mancalaBoard[mancalaPitIndex].setStones(0); //removes the stone from the pit you start with 
-       for(int i = stonesInHand; i >0 ;i--)
-       { 
-           mancalaBoard[mancalaPitIndex+= 1].addStones(); // if it isnt a mancala well
+    public void printBoard(Pit[] board)
+    {
+    	for (int i = 0; i < 7; i++)
+        {
+             System.out.print(Integer.toString(board[i].getStones()));
+        }
+        System.out.print("\n");
+        for (int i = board.length-1; i > 6; i--)
+        {
+              System.out.print(Integer.toString(board[i].getStones()));
+        }
+        System.out.print("\n");
+    }
+    public void move(int mancalaPitIndex)
+    {
+    	//TODO make sure that a rock isnt placed in the enemy mancala or landing in the mancala clears the opposite pit
+    	int stonesInHand = mancalaBoard[mancalaPitIndex].getStones();
+    	int mancalaPitIndex2=0;
+    	mancalaBoard[mancalaPitIndex].setStones(0); //removes the stone from the pit you start with 
+    	for(int i = stonesInHand; i >0 ;i--)
+    	{ 
+    		if(PLAYER_A_MANCALA==mancalaPitIndex && player==false)
+    	   	{   //remove the extra stone added to the enemy players mancala
+    		   	mancalaBoard[mancalaPitIndex].setStones(mancalaBoard[mancalaPitIndex].getStones()-1);
+    	   	} 
+    	   	if(PLAYER_B_MANCALA==mancalaPitIndex && player==true)
+    	   	{   //remove the extra stone added to the enemy players mancala
+    		   	mancalaBoard[mancalaPitIndex].setStones(mancalaBoard[mancalaPitIndex].getStones()-1);
+    	   	}
+    	   	mancalaBoard[mancalaPitIndex+= 1].addStones(); // if it isnt a mancala well
           
-           //if player does not own the mancala, he must skip the mancalla
-           if(i==1)
-           {
-        	   if((PLAYER_A_MANCALA == mancalaPitIndex)&&(player==true)) //if the last stone is in the players mancalapit, then he gets to go again
-        	   { //if you end in your mancala
-        		   		mancalaBoard[mancalaPitIndex].addStones();
-        	   			move(mancalaPitIndex2); //play again
-        	   }
-        	   if((PLAYER_B_MANCALA == mancalaPitIndex)&&(player==false))
-        	   {
-        		   mancalaBoard[mancalaPitIndex].addStones();
-        		   move(mancalaPitIndex2); //play again
-        	   }
-           }
-       }
+    	   	//if player does not own the mancala, he must skip the mancalla
+    	   	if(i==1) //if it is the last stone
+    	   	{
+    	   		if((PLAYER_A_MANCALA == mancalaPitIndex)&&(player==true)) //if the last stone is in the players mancalapit, then he gets to go again
+    	   		{ 	//if you end in your mancala
+        		   	mancalaBoard[mancalaPitIndex].addStones();
+        	   		move(mancalaPitIndex2); //play again
+    	   		}
+    	   		if((PLAYER_B_MANCALA == mancalaPitIndex)&&(player==false))
+    	   		{
+    	   			mancalaBoard[mancalaPitIndex].addStones();
+    	   			move(mancalaPitIndex2); //play again
+    	   		}
+    	   	}
+    	}
        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-       if(mancalaBoard[mancalaPitIndex].getStones()==1) //you land on an empty spot
+       if((mancalaBoard[mancalaPitIndex].getStones()==1)/*&&(mancalaPitIndex!=PLAYER_A_MANCALA)&&(mancalaPitIndex!=PLAYER_B_MANCALA)*/) //you land on an empty spot and it is not a mancala
        {
     	   if((player)&&(mancalaPitIndex!=PLAYER_A_MANCALA)) //if it is player A and is not his mancala
     	   {								  //CHANGE THIS SHITTY NAME
